2914640c83bc622326d45d8b8fbb387144bf7eed
SystemGlitch <jeremy.la@outlook.fr>
2020-10-21 11:17:04 +0200

    Set email and password unique

--- http/controller/user/request.go
+++ http/controller/user/request.go
@@ -5,16 +5,16 @@ import "github.com/System-Glitch/goyave/v3/validation"
 var (
 	// InsertRequest validates Post requests for users
 	InsertRequest validation.RuleSet = validation.RuleSet{
-		"email":    {"required", "string", "email", "between:3,100"}, // TODO implement unique validation
-		"username": {"required", "string", "between:3,100"},
+		"email":    {"required", "string", "email", "between:3,100", "unique:users"},
+		"username": {"required", "string", "between:3,100", "unique:users"},
 		"image":    {"nullable", "file", "image", "max:2048", "count:1"},
 		"password": {"required", "string", "between:6,100"}, // TODO implement password validation
 	}
 
 	// UpdateRequest validates Put requests for users
 	UpdateRequest validation.RuleSet = validation.RuleSet{
-		"email":    {"nullable", "string", "email", "between:3,100"},
-		"username": {"nullable", "string", "between:3,100"},
+		"email":    {"nullable", "string", "email", "between:3,100", "unique:users"},
+		"username": {"nullable", "string", "between:3,100", "unique:users"},
 		"image":    {"nullable", "file", "image", "max:2048", "count:1"},
 		"password": {"nullable", "string", "between:6,100"},
 	}
